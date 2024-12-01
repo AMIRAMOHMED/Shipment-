78b6225495fbf2897d65e7a4d87b40e3f4c647e2
Amiramohamed <amiramohmaed887@gmail.com>
2024-12-02 01:17:31 +0200

    made setup of networking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,11 @@ kapt {
 }
 
 dependencies {
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.airbnb.android:lottie:6.0.1")
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
