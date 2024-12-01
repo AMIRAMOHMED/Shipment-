398b9582c9c28e2d9ee25ccef9c66577a5439806
Amiramohamed <amiramohmaed887@gmail.com>
2024-12-02 01:01:50 +0200

    made ui of register screen and setup of fonts and main color i will used

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ kapt {
 }
 
 dependencies {
+    implementation("com.airbnb.android:lottie:6.0.1")
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
     implementation(libs.androidx.core.ktx)
